644a759a73116f1bf2babdb235e448bf305d36b2
Szymon <33368441+SKpracownia1@users.noreply.github.com>
2018-04-06 11:52:36 +0200

    Add files via upload

---
+++ graf.py
@@ -0,0 +1,30 @@
+#-*- coding: utf-8 -*-
+# Autor: Szymon Kącki gr. 2 196348
+
+def stopnie(m):
+	licznik=0
+	st_grafu=0
+	for i in m:
+		print("Stopień wierzchołka ",licznik,"=",sum(i))
+		if (sum(i)>=st_grafu):
+			st_grafu=sum(i)
+		licznik+=1
+	print("Stopień grafu= ",st_grafu)
+	
+
+n=input("Podaj liczbę wierzchołków:")
+m=input("Podaj liczbę krawędzi:")
+
+matrix = [[0]*int(n) for i in range(int(n))]
+
+print("Podaj wierzchołki początkowe i końcowe krawędzi(oddzielone spacją):")
+
+for j in range (int(m)):
+	a,b=input().split(' ')
+	matrix[int(a)][int(b)]=1
+	matrix[int(b)][int(a)]=1
+
+stopnie(matrix)
+	
+	
+
